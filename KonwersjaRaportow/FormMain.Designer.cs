0d8787d8ffcc9c0fad178dea42e1765a387220aa
Grzegorz Gogolewski <grzegorz.gogolewski@gmail.com>
2019-08-25 21:08:56 +0200

    Add project files.

---
+++ KonwersjaRaportow/FormMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace KonwersjaRaportow
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nazwa pliku";
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(16, 30);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(526, 20);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStart.Location = new System.Drawing.Point(467, 56);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "buttonStart";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelect.Location = new System.Drawing.Point(386, 56);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelect.TabIndex = 3;
+            this.buttonSelect.Text = "buttonSelect";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.ButtonSelect_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 95);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
+
---
+++ KonwersjaRaportow/FormMain.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace KonwersjaRaportow
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Text = @"Konwersja raportów";
+            buttonStart.Text = @"Start";
+            buttonSelect.Text = "Wybierz plik";
+        }
+
+        private void ButtonSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Pliki CSV (*.csv)|*.csv|Wszystie pliki (*.*)|*.*"
+            };
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFileName.Text = dlg.FileName;
+            }
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            ExcelTextFormat format = new ExcelTextFormat
+            {
+                Delimiter = '\t',
+                Culture = new CultureInfo(Thread.CurrentThread.CurrentCulture.ToString())
+                {
+                    DateTimeFormat = {ShortDatePattern = "yyyy-mm-dd"}
+                },
+                Encoding = new UTF8Encoding()
+            };
+
+            //read the CSV file from disk
+            FileInfo file = new FileInfo(textBoxFileName.Text);
+
+            //create a new Excel package
+            using (ExcelPackage excelPackage = new ExcelPackage())
+            {
+                //create a WorkSheet
+                ExcelWorksheet worksheetAll = excelPackage.Workbook.Worksheets.Add("wszystkie");
+
+                //load the CSV data into cell A1
+                worksheetAll.Cells["A1"].LoadFromText(file, format);
+
+                int columnsCount = worksheetAll.Dimension.Columns;
+
+                List<string> numeryKOntroli = new List<string>();
+
+                int rowsCounter = 1;
+
+                do
+                {
+                    numeryKOntroli.Add(worksheetAll.Cells["A" + rowsCounter].Value.ToString());
+                    rowsCounter++;
+
+                } while (worksheetAll.Cells["A" + rowsCounter].Value != null);
+
+                numeryKOntroli = numeryKOntroli.Distinct().ToList(); // unikalna lista kontroli
+
+                foreach (string numerKontroli in numeryKOntroli)
+                {
+                    ExcelWorksheet arkusz = excelPackage.Workbook.Worksheets.Add(numerKontroli);
+
+                    int destRow = 1;
+                    rowsCounter = 1;
+                    do
+                    {
+                        if (worksheetAll.Cells[rowsCounter, 1].Value.ToString() == numerKontroli)
+                        {
+                            worksheetAll.Cells[rowsCounter, 1, rowsCounter, columnsCount].Copy(arkusz.Cells[destRow++, 1]);
+                        }
+
+                        rowsCounter++;
+
+                    } while (worksheetAll.Cells["A" + rowsCounter].Value != null);
+
+                    arkusz.Cells.AutoFitColumns();
+                }
+
+
+                excelPackage.SaveAs(new FileInfo(Path.Combine(Path.GetDirectoryName(textBoxFileName.Text) ?? throw new InvalidOperationException(), Path.GetFileNameWithoutExtension(textBoxFileName.Text) + ".xlsx")));
+            }
+
+            Process.Start(Path.Combine(Path.GetDirectoryName(textBoxFileName.Text) ?? throw new InvalidOperationException(), Path.GetFileNameWithoutExtension(textBoxFileName.Text) + ".xlsx"));
+
+            MessageBox.Show("Koniec", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
